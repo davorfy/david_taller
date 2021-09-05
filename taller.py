b38c671b997aa9fdce9fb71004eb7afdb13f9f7b
David Villa <davidvillasotomayor@gmail.com>
2021-09-04 23:17:52 -0500

    Segundo valor de y

--- taller.py
+++ taller.py
@@ -9,3 +9,10 @@ Created on Sat Sep  4 22:49:40 2021
 def valory():
     y = ( ( 5 + 2 - 5)**2 * 5 + 8 / 2 - 30) / 2 * 5 - 3
     return y
+
+def valor2y():
+    z = 5
+    n = 3
+    m = z - n
+    y = (((z + 2 - n)**2 * m + 8 / 2 - 30) / 2 * 5 - 3 )**5 + 15 * 3 - 9 / 3
+    return y
